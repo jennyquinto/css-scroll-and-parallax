299a412984f93d4d6ee40b059f5b7ea3cef1556a
jennyquinto <vanessaquinto719@gmail.com>
2024-02-03 02:43:19 -0500

    script header content opacity

--- js/script.js
+++ js/script.js
@@ -17,7 +17,8 @@ $(function () {
             ? headerCue.classList.add('d-none')
             : headerCue.classList.remove('d-none')
 
-        headerContent.style.transform = `translateY(-${top / 1}px)`
+        headerContent.style.transform = `translateY(-${top / 1.5}px)`
+        headerContent.style.opacity = 1 - Math.max(top / (window.innerHeight * .2), 0)
 
 
         window.requestAnimationFrame(moveHeader)
